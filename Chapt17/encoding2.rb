d95a32af44fae7d7a9b2826142dfa36fff7da4f1
Jason Ruchti <jasonruchti@Jasons-MacBook-Pro-2.local>
2014-06-08 16:04:42 -0600

    Programming Ruby Chapter #17
    Discussion of character encoding schemes (e.g. ASCII, UTF) and Ruby support.

---
+++ Chapt17/encoding2.rb
@@ -0,0 +1,4 @@
+# encoding: utf-8
+# Demonstrates non-standard encodings
+π = 3.14158
+puts "π = #{π}"
---
+++ Chapt17/encoding3.rb
@@ -0,0 +1,4 @@
+# encoding: utf-8
+# Demonstrates getting raw bytes from a string
+
+puts "pi: \u03c0".bytes.to_a
